b62a4740a0cfdefa4cc778ec58693f8d0de3e1b3
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-12-31 23:40:42 +0600

    Practice String DataType In TypeScript

--- SimpleTypeScript.js
+++ SimpleTypeScript.js
@@ -9,3 +9,9 @@ var binary = 10;
 var octal = 484;
 var float = 3.14;
 console.log({ decimal: decimal, hex: hex, binary: binary, octal: octal, float: float });
+// String 
+var color = 'blue';
+var fullName = 'Ashiqur Rahman';
+var age = 27;
+var sentence = "Hello I'm ".concat(fullName, ", I'm ").concat(age, " years old");
+console.log(sentence);
--- SimpleTypeScript.ts
+++ SimpleTypeScript.ts
@@ -11,3 +11,10 @@ let binary: number = 0b1010;
 let octal: number = 0o744;
 let float: number = 3.14;
 console.log({ decimal, hex, binary, octal, float });
+
+// String 
+let color: string = 'blue';
+let fullName: string = 'Ashiqur Rahman';
+let age: number = 27;
+let sentence: string = `Hello I'm ${fullName}, I'm ${age} years old`
+console.log(sentence);
