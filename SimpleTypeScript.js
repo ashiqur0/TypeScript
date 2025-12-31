d1d036eee3d98c462f5699aef00c29b124909ac8
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-12-31 23:32:18 +0600

    Practice Boolean DataType In TypeScript

---
+++ SimpleTypeScript.js
@@ -0,0 +1,4 @@
+// Boolean
+var isActive = true;
+var hasPermission = false;
+console.log({ isActive: isActive, hasPermission: hasPermission });
---
+++ SimpleTypeScript.ts
@@ -0,0 +1,5 @@
+// Boolean
+let isActive: boolean = true;
+let hasPermission: boolean = false;
+
+console.log({ isActive, hasPermission });
