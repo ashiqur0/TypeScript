3f7078d674c317c415bb4719b7632c75d37d3d1d
ashiqur0 <ashiqurrahmans2018@gmail.com>
2026-01-24 03:45:19 +0600

    Practice Interface With Object Type In TypeScript

---
+++ src/interface.ts
@@ -0,0 +1,28 @@
+// Interface Type: Similar as Type Alias. Use for non-primitive types like object, array, function, class
+
+interface User {
+    name: string;
+    age: number;
+    isAdmin: boolean;
+    getDetails: () => string; // method signature
+}
+
+interface Role {
+    role: string;
+    permissions: string[];
+}
+
+interface UserWithRole extends User, Role {} // Interface Inheritance
+
+const user1: UserWithRole = {
+    name: "John Doe",
+    age: 30,
+    isAdmin: true,
+    getDetails: function (): string {
+        return `Name: ${this.name}, Age: ${this.age}, Admin: ${this.isAdmin}`;
+    },
+    role: "Administrator",
+    permissions: ["read", "write", "execute"]
+};
+
+console.log(user1.getDetails());
