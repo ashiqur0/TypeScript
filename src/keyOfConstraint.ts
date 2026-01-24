b7145620a5fcc7adb2ab029f261a0b3f80049497
ashiqur0 <ashiqurrahmans2018@gmail.com>
2026-01-24 22:47:41 +0600

    Practice keyof Constraint With Generic Function Type In TypeScript

--- src/keyOfConstraint.ts
+++ src/keyOfConstraint.ts
@@ -14,3 +14,42 @@ const myVehicle1: MyVehicle1 = 'car';
 const myVehicle2: MyVehicle2 = 'yacht';
 // const myVehicle3: MyVehicle2 = 'ship'; // error
 
+type User = {
+    id: number,
+    name: string,
+    address: {
+        city: string
+    }
+}
+
+const user: User = {
+    id: 1009,
+    name: "Ashiqur Rahman",
+    address: {
+        city: 'Bogura'
+    }
+}
+const myName1 = user.name;      // dot notation
+const myName2 = user['name'];   // bracket notation
+
+const getPropertyFromObject = <T>(obj: T, key: keyof T) => {
+    return obj[key];
+}
+
+const result = getPropertyFromObject(user, 'id');
+// const result2 = getPropertyFromObject(user, 'emni'); // error
+console.log(result); // 1009
+
+const product = {
+    brand: 'HP'
+}
+
+const student = {
+    name: 'Ashiqur Rahman',
+    id: 1009
+}
+
+const result3 = getPropertyFromObject(product, 'brand');
+const result4 = getPropertyFromObject(student, 'name');
+
+console.log({ result3, result4 });
