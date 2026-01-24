85a84b1b4e24a5e673096443b78ee8750459ed1d
ashiqur0 <ashiqurrahmans2018@gmail.com>
2026-01-24 08:36:08 +0600

    Practice Generic With Array In TypeScript

---
+++ src/generics.ts
@@ -0,0 +1,13 @@
+// Generics: dynamically generalize
+
+// Generic Array
+type GenericArray<T> = Array<T>;
+
+// const names: string[] = ['Arif', 'Tanzid', 'Samiul'];
+const names: GenericArray<string> = ['Arif', 'Tanzid', 'Samiul'];
+const numbers: GenericArray<number> = [29, 48, 1];
+const eligibilities: GenericArray<boolean> = [true, false, false];
+
+console.log(names);
+console.log(numbers);
+console.log(eligibilities);
