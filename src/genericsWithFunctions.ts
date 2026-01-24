3f5edf69a0fa39a6116d67f80d3d395ff90c3af2
ashiqur0 <ashiqurrahmans2018@gmail.com>
2026-01-24 14:17:21 +0600

    Practice Tuple Function With Generics

--- src/genericsWithFunctions.ts
+++ src/genericsWithFunctions.ts
@@ -1,9 +1,17 @@
 // Generics With Functions
 
 const createArrayWithGeneric = <T>(value: T): T[] => [value];
-const arrStr= createArrayWithGeneric<string>('Hello World');
+const arrStr = createArrayWithGeneric<string>('Hello World');
 const arrNum = createArrayWithGeneric<number>(42);
 const arrBol = createArrayWithGeneric<boolean>(true);
 const arrObj = createArrayWithGeneric<object>({ key: 'value' });
 
 console.log(arrStr);
+console.log(arrNum);
+console.log(arrBol);
+console.log(arrObj);
+
+// Generics With Tuples
+const createTupleWithGeneric = <X, Y>(param1: X, param2: Y): [X, Y] => [param1, param2];
+const arrTuple = createTupleWithGeneric<string, number>('Tanzid', 48);
+console.log(arrTuple);
