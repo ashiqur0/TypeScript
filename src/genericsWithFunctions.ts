cb8e13b0fbfd4f463946e321f9e90633e6d01bf6
ashiqur0 <ashiqurrahmans2018@gmail.com>
2026-01-24 14:30:18 +0600

    Practice Generics With Function And Object

--- src/genericsWithFunctions.ts
+++ src/genericsWithFunctions.ts
@@ -15,3 +15,29 @@ console.log(arrObj);
 const createTupleWithGeneric = <X, Y>(param1: X, param2: Y): [X, Y] => [param1, param2];
 const arrTuple = createTupleWithGeneric<string, number>('Tanzid', 48);
 console.log(arrTuple);
+
+// Generics With Object
+const addStudentToCourse = <T>(studentInfo: T): T & { course: string } => {
+    return {
+        course: 'Next Level',
+        ...studentInfo
+    }
+}
+
+const student1 = {
+    name: 'Tanzid Mondol',
+    roll: 48,
+    batch: '23rd',
+    department: 'CSE',
+    university: 'Pundra University of Science & Technology'
+}
+
+const student2 = {
+    name: 'Samiul Islam',
+    roll: 12
+}
+
+const result = addStudentToCourse(student1);
+const result2 = addStudentToCourse(student2);;
+console.log(result);
+console.log(result2);
