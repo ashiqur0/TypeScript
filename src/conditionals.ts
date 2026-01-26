5c4268ae48922a984c1c2a6ac016942988be2fcd
ashiqur0 <ashiqurrahmans2018@gmail.com>
2026-01-26 08:33:37 +0600

    Practice Conditionals With typeof Operator where One Type Depends on Another

--- src/conditionals.ts
+++ src/conditionals.ts
@@ -4,3 +4,13 @@ type A = null;
 type B = undefined;
 
 type C = A extends number ? true : B extends undefined ? true : false;
+
+type RichPeopleVehicles = {
+    bike: string;
+    car: string;
+    ship: string;
+}
+
+type CheckVehicle<T> = T extends keyof RichPeopleVehicles? true : false;
+
+type HasBike = CheckVehicle<'bike'>; // true
