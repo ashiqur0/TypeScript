f742f15c995e326c15764a1cc2eab4d9afa692b0
ashiqur0 <ashiqurrahmans2018@gmail.com>
2026-01-27 15:50:30 +0600

    Practice instanceof Type Guard In TypeScript

--- src/typeguard.ts
+++ src/typeguard.ts
@@ -38,3 +38,53 @@ const adminUser: AdminUser = { name: "Bahadur", role: "Admin" };
 
 console.log(getUserInfo(normalUser)); // Output: I am Sondesh and I am a normal user.
 console.log(getUserInfo(adminUser)); // Output: I am Bahadur and my role is Admin
+
+// instanceof guard: if the object is an instance of a class
+class Person {
+    name: string;
+    constructor(name: string) {
+        this.name = name;
+    }
+
+    getSleep(hours: number) {
+        console.log(`${this.name} is sleeping for ${hours} hours.`);
+    }
+}
+
+class Student extends Person {
+    constructor(name: string) {
+        super(name);
+    }
+
+    doStudy(numberOfHours: number) {
+        console.log(`${this.name} is studying for ${numberOfHours} hours.`);
+    }
+}
+
+class Teacher extends Person {
+    constructor(name: string) {
+        super(name);
+    }
+
+    takeClass(numberOfHours: number) {
+        console.log(`${this.name} is teaching for ${numberOfHours} hours.`);
+    }
+}
+
+const getUserInfo2 = (user: Person) => {
+    if (user instanceof Student) {
+        user.doStudy(5);
+    } else if (user instanceof Teacher) {
+        user.takeClass(4);
+    } else {
+        user.getSleep(8);
+    }
+};
+
+const student1 = new Student("Mr. Jack");
+const teacher1 = new Teacher("Miss. Jenny");
+const person1 = new Person("John Doe");
+
+getUserInfo2(student1); // Output: Mr. Jack is studying for 5 hours.
+getUserInfo2(teacher1); // Output: Miss. Jenny is teaching for 4 hours.
+getUserInfo2(person1);  // Output: John Doe is sleeping for 8 hours.
