b5552f1d2f031c774939fb4047682848338fa04b
ashiqur0 <ashiqurrahmans2018@gmail.com>
2026-01-27 22:45:36 +0600

    Practice Abstraction Using Abstract Class

--- src/abstruction.ts
+++ src/abstruction.ts
@@ -1,5 +1,6 @@
 // Abstraction: This module defines abstract classes and interfaces for the application.
 
+// Way1: using interface
 interface MediaPlayer {
     play(): void;
     pause(): void;
@@ -19,6 +20,31 @@ class MusicPlayer implements MediaPlayer {
 }
 
 const myMusicPlayer = new MusicPlayer();
-myMusicPlayer.play();
-myMusicPlayer.pause();
-myMusicPlayer.stop();
+// myMusicPlayer.play();
+// myMusicPlayer.pause();
+// myMusicPlayer.stop();
+
+
+// Way2: using abstract class
+abstract class MediaPlayer2 {
+    abstract play(): void;
+    abstract pause(): void;
+    abstract stop(): void;
+}
+
+class MusicPlayer2 extends MediaPlayer2 {
+    play(): void {
+        console.log("Playing music...");
+    }
+    pause(): void {
+        console.log("Pausing music...");
+    }
+    stop(): void {
+        console.log("Stopping music...");
+    }
+}
+
+const myMusicPlayer2 = new MusicPlayer();
+myMusicPlayer2.play();
+myMusicPlayer2.pause();
+myMusicPlayer2.stop();
