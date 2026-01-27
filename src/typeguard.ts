75b4ffa1d4ae7eb7b71451ed8c975559621f0033
ashiqur0 <ashiqurrahmans2018@gmail.com>
2026-01-27 08:45:40 +0600

    Practice in Guard In TypeScript

--- src/typeguard.ts
+++ src/typeguard.ts
@@ -1,7 +1,9 @@
 // Type Guard: A type guard is a runtime check that guarantees the type of a variable within a certain scope.
 
-// typeof Type Guard
-const add = (num1:number | string, num2:number | string) => {
+// typeof Type Guard / type narrowing
+type AlphaNumeric = number | string;
+
+const add = (num1: AlphaNumeric, num2: AlphaNumeric) => {
     if (typeof num1 === 'number' && typeof num2 === 'number') {
         return num1 + num2;
     } else {
@@ -12,3 +14,27 @@ const add = (num1:number | string, num2:number | string) => {
 console.log(add(5, 10)); // Output: 15
 console.log(add("Hello, ", "World!")); // Output: Hello, World!
 console.log(add(5, " apples")); // Output: 5 apples
+
+// in guard: if the property exists in the object
+type NormalUser = {
+    name: string;
+}
+
+type AdminUser = {
+    name: string;
+    role: "Admin";
+}
+
+const getUserInfo = (user: NormalUser | AdminUser) => {
+    if ('role' in user) {
+        return `I am ${user.name} and my role is ${user.role}`;
+    } else {
+        return `I am ${user.name} and I am a normal user.`;
+    }
+}
+
+const normalUser: NormalUser = { name: "Sondesh" };
+const adminUser: AdminUser = { name: "Bahadur", role: "Admin" };
+
+console.log(getUserInfo(normalUser)); // Output: I am Sondesh and I am a normal user.
+console.log(getUserInfo(adminUser)); // Output: I am Bahadur and my role is Admin
