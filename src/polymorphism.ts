4747ecd4867db37c6a061e856082e783d188f872
ashiqur0 <ashiqurrahmans2018@gmail.com>
2026-01-27 22:01:08 +0600

    Practice Polymorphism In TypeScript

---
+++ src/polymorphism.ts
@@ -0,0 +1,65 @@
+// Polymorphism: the ability of different classes to be treated as instances of the same class through a common interface.
+
+class Person {
+    getSleep() {
+        console.log("I'm a normal happy person. I need 8 hours of sleep.")
+    }
+}
+
+class Student extends Person {
+    getSleep() {
+        console.log("I'm a student. I need 7 hours of sleep.")
+    }
+}
+
+class Developer extends Person {
+    getSleep() {
+        console.log("I'm a developer. I need 6 hours of sleep.")
+    }
+}
+
+const getSleepingHours = (person: Person) => {
+    person.getSleep();
+}
+
+const person1 = new Person();
+const student1 = new Student();
+const developer1 = new Developer();
+
+getSleepingHours(person1);
+getSleepingHours(student1);
+getSleepingHours(developer1);
+
+class Shape {
+    getArea(): number {
+        return 0;
+    }
+}
+
+class Circle extends Shape {
+    radius: number;
+
+    constructor(radius: number) {
+        super();
+        this.radius = radius;
+    }
+
+    getArea(): number {
+        return Math.PI * this.radius * this.radius;
+    }
+}
+
+class Rectangle extends Shape {
+    length: number;
+    width: number;
+
+    constructor(length: number, width: number) {
+        super();
+        this.length = length;
+        this.width = width;
+    }
+
+    getArea(): number {
+        return this.length * this.width;
+    }
+}
