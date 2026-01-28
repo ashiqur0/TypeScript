c61724b22ac4e421bb1a5c5a4537fbc06439fff0
ashiqur0 <ashiqurrahmans2018@gmail.com>
2026-01-28 17:46:35 +0600

    Problem Solving With TypeScript: Solve Problem 8 -calculateTotalPrice

---
+++ problemSolvingWithTyoeScript/calculateTotalPrice.ts
@@ -0,0 +1,28 @@
+
+type Products = {
+    name: string,
+    price: number,
+    quantity: number,
+    discount?: number
+}
+
+const calculateTotalPrice = (products: Products[]): number => {
+    return products.reduce((acc, prod) => {
+        const price = prod.price * prod.quantity
+        if (prod?.discount) {
+            acc = price - price * prod.discount / 100 + acc;
+        } else {
+            acc = price;
+        }
+
+        return acc;
+    }, 0);
+}
+
+const products = [
+    { name: 'Pen', price: 10, quantity: 2 },
+    { name: 'Notebook', price: 25, quantity: 3, discount: 10 },
+    { name: 'Bag', price: 50, quantity: 1, discount: 20 },
+];
+
+console.log(calculateTotalPrice(products));
